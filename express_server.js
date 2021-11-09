4505d66896ba079667faaa4ac612d5e91a82dddc
Abdul Said <abdulrahman.asaid@gmail.com>
2021-11-09 01:17:49 +0000

    added partials to the file

--- express_server.js
+++ express_server.js
@@ -17,6 +17,16 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+app.get('/urls', (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  return res.render("urls_index", templateVars);
+});
+
+app.get('/urls/:shortURL', (req, res) => {
+  const templateVars = { shortURL: req.params.shortURL, longURL: req.params.longURL};
+  return res.render("urls_show", templateVars);
+});
+
 app.get('/urls.json', (req, res) => {
   res.json(urlDatabase);
 });
